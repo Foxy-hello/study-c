0e22bceae330f029e7e477fe0655ff4a284ee0ff
Foxy-hello <15005117841@163.com>
2021-11-14 18:01:09 +0800

    he

---
+++ lianxi/练习1.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include "windows.h"
+#include "math.h"
+
+main()
+{
+//	printf("\n\n\t\t===超市购物系统===\n"); 
+//	float Unit_Price=2.5;//定义单价为2.5元 //
+//	int quantity; //定义数量//
+//	printf("面包的单价是：2.5元\n");
+//	printf("请输入购买的数量:"); 
+//	scanf("%d",&quantity);
+//	printf("您的消费金额是:%f",Unit_Price*quantity);
+//	
+//	int n;
+//	printf("请输入任意一个四位整数：\n"); 
+//	scanf("%d",&n);
+//	printf("四个数字的和%d",n%10+n/10%10+n/10/10%10+n/10/10/10%10); 
+//		int a,b,c;
+//	printf("===积分管理===\n请输入三名学生的综合积分\n");
+//	printf("请输入第一个学生的积分:");
+//	scanf("%d",&a);
+//	printf("请输入第二个学生的积分：");
+//	scanf("%d",&b) ;
+//	printf("请输入第三个学生的积分：");
+//	scanf("%d",&c) ;
+//	printf("三个学生的平均积分：%f",(float)(a+b+c)/3); 
+	 
+/*	printf("\n\n\t\t南海公园动漫节\n");
+	int big=68,small=30,b,s;
+	
+	printf("请输入大人人数：");
+	scanf("%d",&b);
+	printf("请输入小朋友人数：");
+	scanf("%d",&s);
+	printf("门票总价格是：%d",b*big+s*small); */
+	
+/*	char A;
+	printf("请输入一个字符");
+	scanf("%c",&A); 
+	printf("这是你输入的大写呢：%c\n",A);
+	printf("这是你输入的字符小写：%c",A+32);  */
+	
+//	int a ,b,c,d,e,f;
+//	 printf("输入小学四年级上周图书馆的借阅记录：\n");
+//	 printf("请输入周一进入阅览室的学生人数:");
+//	 scanf("%d",&a);
+//	 printf("请输入周二进入阅览室的学生人数:");
+//	 scanf("%d",&b);
+//	 printf("请输入周三进入阅览室的学生人数:");
+//	 scanf("%d",&c);
+//	 printf("请输入周四进入阅览室的学生人数:");
+//	 scanf("%d",&d);
+//	 printf("请输入周五进入阅览室的学生人数:");
+//	 scanf("%d",&e);
+//	 printf("上周四年级学生进入阅览室的人数总计：%d",a+b+c+d+e);printf("\t\t\t平均每天进入阅览室的人数是：%d\n",(a+b+c+d+e)/5);
+//	 printf("四年级学生总人数：");
+//	 scanf("%d",&f);
+//	 (a+b+c+d+e)<f/2?printf("进入阅览室的学生人数不足总人数一半，学生需加强阅读能力"):printf("学生有较强的学习意识，值得表扬"); 
+//	 
+	 
+//	int a,b,c,d,e,a1,b1,c1,d1;
+//	char  
+//	printf("五一是假期出游的好时节，输入五一五天的气温\n");
+//	printf("请输入5.1气温数据（摄氏度）\n");
+//	printf("最高气温：");
+//	scanf("%d",&a);
+//	printf("\n最低气温：");
+//	scanf("%d",a1);
+//	printf("5.1号是否有风：");
+
+	
+	
+	
+	
+	
+	
+	
+		  
+	
+
+}
+
